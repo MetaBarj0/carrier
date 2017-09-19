7ed27c80d87b74c50e9f8dd4c17a90457a4f44aa
MetaBarj0 <troctsch.cpp@gmail.com>
2017-09-19 22:22:37 +0200

    gcc build script
    - after a successful build of the toolchain, symlinks with standard
      names are created

--- docker/workspace/gcc/install.sh
+++ docker/workspace/gcc/install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 cd /tmp
+
 mkdir -p /usr/local
+
 tar -xf amd64-linux-musl.tar.xz --directory /usr/local/ && \
   mv /usr/local/amd64-linux-musl/* /usr/local/ && \
-  ln -s /usr/local/bin/amd64-linux-musl-as /usr/local/bin/as && \
   rm -r /usr/local/amd64-linux-musl && \
   rm /tmp/amd64-linux-musl.tar.xz && \
   rm /tmp/install.sh && \
   mv /tmp/forward-command.sh /usr/local/bin/
+
+for f in amd64-linux-musl-* do;
+  ln -s $f $(echo $f | sed 's/amd64-linux-musl-//g')
+done
