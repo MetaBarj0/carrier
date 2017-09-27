d7c200c46ed230e7f950d615e9fa50ab9f820579
MetaBarj0 <troctsch.cpp@gmail.com>
2017-09-27 11:56:30 +0200

    docker/published/nettle
    - change the version from 3.3 to 2.5

--- docker/published/nettle/build.sh
+++ docker/published/nettle/build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-tar -xf nettle-3.3.tar.gz
-cd nettle-3.3
+tar -xf nettle-2.5.tar.gz
+cd nettle-2.5
 mkdir build && cd build
-
 ../configure \
   --prefix=/tmp/install \
+  --disable-assembler \
   --disable-shared \
-  CC='forward-command.sh gcc' CXX='forward-command.sh g++' \
-  CFLAGS='-O3 -s' CXXFLAGS='-O3 -s'
+  CC='forward-command.sh gcc' \
+  CXX='forward-command.sh g++' \
+  CFLAGS='-O3 -s'
+  CXXFLAGS='-O3 -s'
 
 make && make install
