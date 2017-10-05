70d7cb1373f0af7f8540646bc94ad1b85eae2b9b
MetaBarj0 <troctsch.cpp@gmail.com>
2017-10-05 08:26:43 -0700

    bootstrap/gcc installation
    - fixed path of archive extraction

--- docker/workspace/gcc/install.sh
+++ docker/workspace/gcc/install.sh
@@ -5,7 +5,7 @@ cd /tmp
 mkdir -p /usr/local
 
 tar -xf amd64-linux-musl.tar.xz --directory /usr/local/ && \
-  mv /usr/local/amd64-linux-musl/amd64-linux-musl/* /usr/local/ && \
+  mv /usr/local/amd64-linux-musl/* /usr/local/ && \
   rm -r /usr/local/amd64-linux-musl && \
   rm /tmp/amd64-linux-musl.tar.xz && \
   rm /tmp/install.sh && \
