f0a47e8a6ee55daa2972f34308976b9ba29c6767
MetaBarj0 <troctsch.cpp@gmail.com>
2017-10-05 06:27:20 -0700

    workspace/gcc
    - fixed the installation script for the dynamic loader to work

--- docker/workspace/gcc/install.sh
+++ docker/workspace/gcc/install.sh
@@ -11,6 +11,11 @@ tar -xf amd64-linux-musl.tar.xz --directory /usr/local/ && \
   rm /tmp/install.sh && \
   mv /tmp/forward-command.sh /usr/local/bin/
 
+# fix links necessary for the dynamic loader to work
+ln -sf /usr/local/lib/libc.so /usr/local/lib/ld-musl-x86_64.so.1
+mkdir /lib
+ln -sf /usr/local/lib/libc.so /lib/ld-musl-x86_64.so.1
+
 cd /usr/local/bin
 
 for f in amd64-linux-musl-*; do
