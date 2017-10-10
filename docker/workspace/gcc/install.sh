6cf3f7a3c947886580c93850e5190aca4461fc65
MetaBarj0 <troctsch.cpp@gmail.com>
2017-10-10 12:15:34 +0200

    docker/workspace/gcc
    - updated installation of the toolchain

--- docker/workspace/gcc/install.sh
+++ docker/workspace/gcc/install.sh
@@ -2,19 +2,14 @@
 
 cd /tmp
 
-mkdir -p /usr/local
-
-tar -xf amd64-linux-musl.tar.xz --directory /usr/local/ && \
-  mv /usr/local/amd64-linux-musl/* /usr/local/ && \
-  rm -r /usr/local/amd64-linux-musl && \
+tar -xf amd64-linux-musl.tar.xz --directory / && \
   rm /tmp/amd64-linux-musl.tar.xz && \
-  rm /tmp/install.sh && \
-  mv /tmp/forward-command.sh /usr/local/bin/
+  rm /tmp/install.sh
 
 # fix links necessary for the dynamic loader to work
-ln -sf /usr/local/lib/libc.so /usr/local/lib/ld-musl-x86_64.so.1
-mkdir /lib
-ln -sf /usr/local/lib/libc.so /lib/ld-musl-x86_64.so.1
+# ln -sf /usr/local/lib/libc.so /usr/local/lib/ld-musl-x86_64.so.1
+# mkdir /lib
+# ln -sf /usr/local/lib/libc.so /lib/ld-musl-x86_64.so.1
 
 cd /usr/local/bin
 
