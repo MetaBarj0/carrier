84a83eb8417b59ed5abe8a273dfdd961cb52825b
MetaBarj0 <troctsch.cpp@gmail.com>
2017-10-10 12:25:17 +0200

    docker/workspace/gcc
    - fixed links for toolchain execution

--- docker/workspace/gcc/install.sh
+++ docker/workspace/gcc/install.sh
@@ -7,9 +7,8 @@ tar -xf amd64-linux-musl.tar.xz --directory / && \
   rm /tmp/install.sh
 
 # fix links necessary for the dynamic loader to work
-# ln -sf /usr/local/lib/libc.so /usr/local/lib/ld-musl-x86_64.so.1
-# mkdir /lib
-# ln -sf /usr/local/lib/libc.so /lib/ld-musl-x86_64.so.1
+ln -sf /usr/local/lib/libc.so /usr/local/lib/ld-musl-x86_64.so.1
+cp -P /usr/local/lib/ld-musl-x86_64.so.1 /lib/
 
 cd /usr/local/bin
 
