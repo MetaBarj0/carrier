39acf7009cb9bf7b9b4cfd20e5baaa02926974a5
MetaBarj0 <troctsch@gmail.com>
2017-10-15 00:21:08 +0000

    docker/published/icu
    - added icu4c

---
+++ docker/published/23-icu4c/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+tar -xf icu4c-59_1-src.tar.xz
+cd icu/source
+mkdir build && cd build
+
+../configure \
+  --prefix=/tmp/install \
+  CFLAGS='-O3 -s' \
+  CXXFLAGS='-O3 -s' \
+  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
+
+# small fix about i18n build
+ln -s /usr/local/include/locale.h /usr/local/include/xlocale.h
+
+# Calculates the optimal job count
+JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j $JOBS && make install
+
+# relocate installed libraries
+find /tmp/install/lib -type f -name '*.la' -exec \
+  sed -i'' 's/\/tmp\/install\//\/usr\/local\//g' {} \;
