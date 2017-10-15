dcf1cd807d08e59ad5681b56b6bba9b48acacf5d
MetaBarj0 <troctsch@gmail.com>
2017-10-15 00:19:03 +0000

    docker/published/curl
    - added the stuff related to moved libraries
    - removed vim swap files

--- docker/published/11-curl/build.sh
+++ docker/published/11-curl/build.sh
@@ -14,3 +14,7 @@ mkdir build && cd build
 JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
 
 make -j $JOBS && make install
+
+# relocate installed libraries
+find /tmp/install/lib -type f -name '*.la' -exec \
+  sed -i'' 's/\/tmp\/install\//\/usr\/local\//g' {} \;
