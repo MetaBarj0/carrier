89a592494eab922060dc0cdf0f144461dd9b4f93
MetaBarj0 <troctsch.cpp@gmail.com>
2017-10-30 21:50:58 +0100

    docker
    - added a helper script to build them all

---
+++ docker/build_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# build all images except the bootstrap that must be constructed manually
+
+# get this script directory and file name, in case it is executed out of his parent directory
+pushd $(dirname $0) > /dev/null
+
+script_dir=$(pwd -P)
+script_name=$(basename $0)
+
+script=${script_dir}/${script_name}
+
+popd > /dev/null
+
+# browse each image directory in order and then, build each image
+for d in $(sort <<< "$(find $script_dir -type d -maxdepth 1 ! -name 00-bootstrap ! -name docker ! -name .)"); do
+  # get only the last part of the directory path
+  image_name=$(basename $d)
+  # replace numerical order by the repository owner
+  image_name=$(sed -E 's/[0-9]{2}-(.+)/metabarj0\/\1/' <<< $image_name)
+  # finally, build the image
+  docker build --squash -t $image_name $d
+done
