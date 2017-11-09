a31495e31e4033bbed8f869f2fc130bbf8591833
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-09 20:17:10 +0000

    docker/ncurses
    - overall build improve

--- docker/08-ncurses/build.sh
+++ docker/08-ncurses/build.sh
@@ -5,10 +5,16 @@ mkdir build && cd build
 
 ../configure \
   --prefix=/tmp/install \
+  --without-ada \
+  --disable-termcap \
+  --disable-rpath-hack \
   --with-shared \
-  --with-cxx-shared \
+  --without-cxx-binding \
   --with-pthread \
   --enable-weak-symbols \
+  --with-terminfo-dirs="/etc/terminfo:/usr/local/share/terminfo" \
+  --enable-pc-files \
+  --enable-widec \
   CFLAGS='-O3 -s' \
   CXXFLAGS='-O3 -s' \
   LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
@@ -21,3 +27,9 @@ make -j $JOBS && make install
 # relocate installed libraries
 find /tmp/install/lib -type f -name '*.la' -exec \
   sed -i'' 's/\/tmp\/install\//\/usr\/local\//g' {} \;
+
+# create links on produced libraries
+cd /tmp/install/lib
+for f in libncurses*; do
+  ln -s $f $(echo $f | sed 's/libncursesw/libncurses/')
+done
