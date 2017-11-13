72ad50f4c53977267f327f91921af0c0bcfee73b
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-13 12:12:13 +0000

    docker/build tools
    - renamed some stuff
    - use local variables in function

--- docker/01-build-tools/functions.sh
+++ docker/01-build-tools/functions.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
 # try to extract each shared object dependecies for a specified file
-tryExtractSharedObjectFromFile() {
+extractNeededSharedObjectsOf() {
   # I need a file as input
-  if [ -z $1 ]; then
+  if [ -z "$1" ]; then
     echo 'No file specified...exiting...'
     return 1
   fi
 
   # get the name of the shared object if the file is binary
-  needed_so_files=$(
-    readelf -d $1 | grep NEEDED | sed -r 's/.+\[(.+)\]$/\1/') 2> /dev/null
+  local needed_so_files=$(
+    readelf -d "$1" 2> /dev/null | grep NEEDED | sed -r 's/.+\[(.+)\]$/\1/')
 
   # shared objects found
   if [ ! -z "$needed_so_files" ]; then
     # gets the absolute path of each shared object and put it in image.dist
-    so_paths=
+    local so_paths=
+    local so_file=
     for so_file in $needed_so_files; do
       # get the absolute path
-      so_path="$(find $PREFIX -name $so_file)"
+      local so_path="$(find $PREFIX -name $so_file)"
 
       # add the file in the list
       so_paths="$so_paths"$'\n'"$so_path"
@@ -33,6 +34,9 @@ tryExtractSharedObjectFromFile() {
       done
     done
 
+    # deduping and sorting
+    so_paths="$(echo "$so_paths" | sort | uniq)"
+
     echo "$so_paths"
   fi
 
@@ -42,11 +46,14 @@ tryExtractSharedObjectFromFile() {
 # collect each dynamic object dependencies in /image.dist file.
 # each file in image.dist is scanned
 collectSharedObjectDependencies() {
+  local x=
+  local so=
   for x in $(cat /image.dist); do
     # directory item, looks for file
     if [ -d $x ]; then
+      local f=
       for f in $(find $x -type f); do
-        so="$(tryExtractSharedObjectFromFile $f)"
+        so="$(extractNeededSharedObjectsOf $f)"
         if [ ! -z "$so" ]; then
 	  # some shared objects have been found
           echo "$so" >> /image.dist
@@ -56,7 +63,7 @@ collectSharedObjectDependencies() {
   
     # file or symlink found
     if [ -f $x -o -L $x ]; then
-      so="$(tryExtractSharedObjectFromFile $x)"
+      so="$(extractNeededSharedObjectsOf $x)"
       if [ ! -z "$so" ]; then
 	  # some shared objects have been found
         echo "$so" >> /image.dist
