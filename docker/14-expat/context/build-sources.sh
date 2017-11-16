c6fef952afff2e9b111d7e2884b1d27bd99bc7a7
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-16 10:50:29 +0000

    docker/expat
    - migrated to the new build system

---
+++ docker/14-expat/context/build-sources.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# extract sources and prepare for build
+tar -xf expat-2.2.4.tar.bz2
+cd expat-2.2.4
+mkdir build && cd build
+
+PREFIX=/usr/local
+
+../configure \
+  --prefix=$PREFIX \
+  CFLAGS='-O3 -s' \
+  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
+
+# Calculates the optimal job count
+JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j $JOBS && make install
+
+# make this image a package
+package
