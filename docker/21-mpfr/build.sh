8d855a348cccef6b741be26f5f14ec787cedc106
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-17 12:23:53 +0000

    docker/mpfr
    - migrated to the new build system

--- docker/21-mpfr/build.sh
+++ docker/21-mpfr/build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-tar -xf mpfr-3.1.6.tar.xz
-cd mpfr-3.1.6
-mkdir build && cd build
 
-../configure \
-  --prefix=/tmp/install \
-  CFLAGS='-O3 -s' \
-  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
+# grab the build script from the build tools
+CURRENT_DIRECTORY=$(pwd -P)
+cd $(dirname $0)
+SCRIPT_DIRECTORY=$(pwd -P)
+BUILD_TOOLS_DIRECTORY=$SCRIPT_DIRECTORY/../01-build-tools
+cd $CURRENT_DIRECTORY
 
-# Calculates the optimal job count
-JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+# if this image require some extra commands (environment vars, volumes...), put
+# them here
+EXTRA_DOCKERFILE_COMMANDS=
 
-make -j $JOBS && make install
-
-# relocate installed libraries
-find /tmp/install/lib -type f -name '*.la' -exec \
-  sed -i'' 's/\/tmp\/install\//\/usr\/local\//g' {} \;
+exec \
+  $BUILD_TOOLS_DIRECTORY/build.sh \
+  metabarj0/mpfr \
+  $SCRIPT_DIRECTORY \
+  "$EXTRA_DOCKERFILE_COMMANDS"
---
+++ docker/21-mpfr/context/build-sources.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#!/bin/sh
+# the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# extract sources and prepare for build
+tar -xf mpfr-3.1.6.tar.xz
+cd mpfr-3.1.6
+mkdir build && cd build
+
+PREFIX=/usr/local
+
+../configure \
+  --prefix=$PREFIX \
+  CFLAGS='-O3 -s' \
+  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
+
+# Calculates the optimal job count
+JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j $JOBS && make install
+
+# make this image a package
+package
