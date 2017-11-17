30676be0c07fd3b1c05e0441b370171e6622947e
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-17 16:33:30 +0000

    docker/isl
    - migrated to the new build system

--- docker/23-isl/build.sh
+++ docker/23-isl/build.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
-tar -xf isl-0.18.tar.xz
-cd isl-0.18
-mkdir build && cd build
 
-../configure \
-  --prefix=/tmp/install \
-  CFLAGS='-O3 -s' \
-  CXXFLAGS='-O3 -s' \
-  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
+# grab the build script from the build tools
+CURRENT_DIRECTORY=$(pwd -P)
+cd $(dirname $0)
+SCRIPT_DIRECTORY=$(pwd -P)
+BUILD_TOOLS_DIRECTORY=$SCRIPT_DIRECTORY/../01-build-tools
+cd $CURRENT_DIRECTORY
 
-# Calculates the optimal job count
-JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+# if this image require some extra commands (environment vars, volumes...), put
+# them here
+EXTRA_DOCKERFILE_COMMANDS=
 
-make -j $JOBS && make install
-
-# relocate installed libraries
-find /tmp/install/lib -type f -name '*.la' -exec \
-  sed -i'' 's/\/tmp\/install\//\/usr\/local\//g' {} \;
-
-# fix prefix in pkgconfig files
-sed -i'' -r 's/^prefix=.*/prefix=\/usr\/local/g' /tmp/install/lib/pkgconfig/isl.pc
+exec \
+  $BUILD_TOOLS_DIRECTORY/build.sh \
+  metabarj0/isl \
+  $SCRIPT_DIRECTORY \
+  "$EXTRA_DOCKERFILE_COMMANDS"
