0cdc7178224ee41ebb4876550968d4b4be36dbbb
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-21 09:21:47 +0000

    docker
    - added readline project
    - added texinfo (weird dependency of gdb build process)
    - migrated gdb to the new build system

---
+++ docker/24_0-readline/context/build-sources.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# extract sources and prepare for build
+tar -xf readline-7.0.tar.gz
+cd readline-7.0
+
+mkdir build && cd build
+
+PREFIX=/usr/local
+
+../configure \
+  --prefix=$PREFIX \
+  --with-curses \
+  CFLAGS='-O3 -s'
+
+# Calculates the optimal job count
+JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j $JOBS && make install
+
+# make this image a package
+package
