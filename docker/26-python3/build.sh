15be184c11a5d847ed0e302a863f916bfd8e43e6
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-23 14:09:40 +0000

    docker/python3
    - migrated to the new build system

--- docker/26-python3/build.sh
+++ docker/26-python3/build.sh
@@ -1,35 +1,18 @@
 #!/bin/sh
-tar -xf Python-3.6.3.tar.xz
-cd Python-3.6.3
-mkdir build && cd build
 
-# copy the custom setup file to enable ssl support
-cp -f Setup Python-3.6.3/Modules/
+# grab the build script from the build tools
+CURRENT_DIRECTORY=$(pwd -P)
+cd $(dirname $0)
+SCRIPT_DIRECTORY=$(pwd -P)
+BUILD_TOOLS_DIRECTORY=$SCRIPT_DIRECTORY/../01-build-tools
+cd $CURRENT_DIRECTORY
 
-../configure \
-  --prefix=/tmp/install \
-  --enable-shared \
-  --enable-optimizations \
-  --with-lto \
-  CFLAGS='-O3 -s' \
-  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
+# if this image require some extra commands (environment vars, volumes...), put
+# them here
+EXTRA_DOCKERFILE_COMMANDS=
 
-# Calculates the optimal job count
-JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
-
-make -j $JOBS && make install
-
-# remove all compiled python files that will contain wrong paths
-find /tmp/install/lib/python3.6 -type f -name '*.pyc' -delete
-
-# changing python path in shabangs
-for f in $(find /tmp/install/bin -exec grep -H '#!/tmp/install' {} \; | sed 's/:.*$//g'); do
-  sed -i'' 's/#!\/tmp\/install/#!\/usr\/local/' $f
-done
-
-# relocate installed libraries
-find /tmp/install/lib -type f -name '*.la' -exec \
-  sed -i'' 's/\/tmp\/install\//\/usr\/local\//g' {} \;
-
-# fix prefix in pkgconfig files
-sed -i'' -r 's/^prefix=.*/prefix=\/usr\/local/g' /tmp/install/lib/pkgconfig/python-3.6.pc
+exec \
+  $BUILD_TOOLS_DIRECTORY/build.sh \
+  metabarj0/python3 \
+  $SCRIPT_DIRECTORY \
+  "$EXTRA_DOCKERFILE_COMMANDS"
---
+++ docker/26-python3/context/build-sources.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# extract sources and prepare for build
+tar -xf Python-3.6.3.tar.xz
+
+# copy the custom setup file to enable ssl support
+cp -f Setup Python-3.6.3/Modules/
+
+cd Python-3.6.3
+mkdir build && cd build
+
+PREFIX=/usr/local
+
+../configure \
+  --prefix=$PREFIX \
+  --enable-shared \
+  --enable-optimizations \
+  --with-lto \
+  CFLAGS='-O3 -s' \
+  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/'
+
+# Calculates the optimal job count
+JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j $JOBS && make install
+
+# remove all compiled python files
+find /usr/local/lib/python3.6 -type f -name '*.pyc' -delete
+
+# make this image a package
+packageIncluding metabarj0/openssl
