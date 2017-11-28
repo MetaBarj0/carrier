3ef871859d205c84cd40dc12db533e1dea338bf6
MetaBarj0 <troctsch.cpp@gmail.com>
2017-11-28 09:19:41 +0000

    docker/go
    - migrated to the new build system
    - brought some fixes and improvement to the build

--- docker/33-go/build.sh
+++ docker/33-go/build.sh
@@ -1,65 +1,21 @@
 #!/bin/sh
 
-# building the bootstrap
-tar -xf go1.4-bootstrap-20170531.tar.gz
-cd go/src
+# grab the build script from the build tools
+CURRENT_DIRECTORY=$(pwd -P)
+cd $(dirname $0)
+SCRIPT_DIRECTORY=$(pwd -P)
+BUILD_TOOLS_DIRECTORY=$SCRIPT_DIRECTORY/../01-build-tools
+cd $CURRENT_DIRECTORY
 
-# use sh instead of bash
-for f in $(find . -type f -exec grep -EH '#!\s*\/usr\/bin\/env\s*bash' {} \; | sed 's/:.*//'); do
-  sed -i'' -r 's/#!\s*\/usr\/bin\/env\s*bash/#!\/bin\/sh/g' $f
-done
+# if this image require some extra commands (environment vars, volumes...), put
+# them here
+EXTRA_DOCKERFILE_COMMANDS="$(cat << EOI
+ENV PATH /usr/local/go/bin:\${PATH}
+EOI
+)"
 
-# this directory is necessary
-mkdir -p /var/tmp
-
-GOARCH='amd64' \
-  CFLAGS='-O3 -s' \
-  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/,-rpath,/usr/local/amd64-linux-musl/lib64/,-rpath-link,/usr/local/amd64-linux-musl/lib64/' \
-  CGO_ENABLED=0 \
-  ./make.bash --no-clean
-
-cd /tmp/go
-
-# manual installation needed
-mkdir -p /tmp/go-bootstrap
-cp -a src pkg bin /tmp/go-bootstrap
-
-cd /tmp/go-bootstrap
-# don't need these files
-find src/ -type f -name '*_test.go' -delete
-find src/ -type f -name '*.bash' -delete
-find src/ -type d -name 'testdata' -exec rm -rf {} \;
-
-# bootstrap built, build last version using it
-cd /tmp
-rm -rf go
-
-tar -xf go1.9.2.src.tar.gz
-cd go/src
-
-# use sh instead of bash
-for f in $(find . -type f -exec grep -EH '#!\s*\/usr\/bin\/env\s*bash' {} \; | sed 's/:.*//'); do
-  sed -i'' -r 's/#!\s*\/usr\/bin\/env\s*bash/#!\/bin\/sh/g' $f
-done
-
-GOROOT_FINAL='/usr/local/go' GOARCH='amd64' GOOS='linux' GOROOT_BOOTSTRAP=/tmp/go-bootstrap ./make.bash
-
-# create installation directory hierarchy and move built files
-mkdir -p /usr/local/go
-cd /tmp/go
-mv bin/ pkg/ src/ doc/ /usr/local/go/
-cd /usr/local/go
-
-# stripping binaries
-for f in bin/*; do
-  strip $f
-done
-for f in pkg/tool/*/*; do
-  strip $f
-done
-
-# removing unnecessary files
-find src/ -type f -name '*_test.go' -delete
-find src/ -type f -name '*.bash' -delete
-find src/ -type d -name 'testdata' -exec rm -rf {} \;
-rm -rf pkg/bootstrap
+exec \
+  $BUILD_TOOLS_DIRECTORY/build.sh \
+  metabarj0/go \
+  $SCRIPT_DIRECTORY \
+  "$EXTRA_DOCKERFILE_COMMANDS"
---
+++ docker/33-go/context/build-sources.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+echo pause && read
+# the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# building the bootstrap
+tar -xf go1.4-bootstrap-20170531.tar.gz
+cd go/src
+
+# use sh instead of bash
+for f in $(find . -type f -exec grep -EH '#!\s*\/usr\/bin\/env\s*bash' {} \; | sed 's/:.*//'); do
+  sed -i'' -r 's/#!\s*\/usr\/bin\/env\s*bash/#!\/bin\/sh/g' $f
+done
+
+# this directory is necessary
+mkdir -p /var/tmp
+
+GOARCH='amd64' \
+  CFLAGS='-O3 -s' \
+  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/,-rpath,/usr/local/amd64-linux-musl/lib64/,-rpath-link,/usr/local/amd64-linux-musl/lib64/' \
+  CGO_ENABLED=0 \
+  ./make.bash --no-clean
+
+cd /tmp/go
+
+# manual installation needed
+mkdir -p /tmp/go-bootstrap
+mv src pkg bin /tmp/go-bootstrap
+
+cd /tmp/go-bootstrap
+
+# bootstrap built, build last version using it
+cd /tmp
+rm -rf go
+
+tar -xf go1.9.2.src.tar.gz
+cd go/src
+
+# use sh instead of bash
+for f in $(find . -type f -exec grep -EH '#!\s*\/usr\/bin\/env\s*bash' {} \; | sed 's/:.*//'); do
+  sed -i'' -r 's/#!\s*\/usr\/bin\/env\s*bash/#!\/bin\/sh/g' $f
+done
+
+PREFIX=/usr/local/go
+
+GOROOT_FINAL=$PREFIX \
+  GOARCH='amd64' \
+  GOOS='linux' \
+  GOROOT_BOOTSTRAP=/tmp/go-bootstrap \
+  ./make.bash
+
+# create installation directory hierarchy and move built files
+mkdir -p $PREFIX
+cd /tmp/go
+mv bin/ pkg/ src/ doc/ $PREFIX
+cd $PREFIX
+
+# stripping binaries
+for f in bin/*; do
+  strip $f
+done
+for f in pkg/tool/*/*; do
+  strip $f
+done
+
+# removing unnecessary files
+find src/ -type f -name '*_test.go' >> /tmp/unneeded_files
+find src/ -type f -name '*.bash'  >> /tmp/unneeded_files
+find src/ -type d -name 'testdata' >> /tmp/unneeded_files
+
+for x in $(cat /tmp/unneeded_files); do
+  rm -rf $x
+done
+
+rm -rf pkg/bootstrap
+
+# make this image a package
+package
