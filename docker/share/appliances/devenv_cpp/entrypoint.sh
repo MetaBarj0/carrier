455a1783a1354762322aecef8fdccc8e92f68046
MetaBarj0 <troctsch.cpp@gmail.com>
2018-01-28 18:31:45 +0100

    docker - devenv_cpp
    - refined more this appliance
    - for some reasons I ignore, the entrypoint specified in the
      docker-compose seems to be executed, but there is no output on the
      console...

--- docker/share/appliances/devenv_cpp/entrypoint.sh
+++ docker/share/appliances/devenv_cpp/entrypoint.sh
@@ -4,6 +4,15 @@ if [ -z "$1" ]; then
   set -- sh
 fi
 
-# exploit and cleanup sensible environment data
+# check the appliance, warn the user if something is missing
+if [ ! -f '.ssh/id_rsa' ] \
+   || [ ! -f '.ssh/id_rsa.pub' ]; then
+  cat << EOI
+********************************************************************************
+* Warning: Cannot find ssh public or secret key. Note that you need those      *
+*          keys in order to operate with git through the SSH protocol.         *
+********************************************************************************
+EOI
+fi
 
 exec "$(echo "$@")"
