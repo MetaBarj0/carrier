bccbc47c900df53c798367dd4d4969c6dbfdb978
MetaBarj0 <troctsch.cpp@gmail.com>
2018-02-08 18:46:39 +0100

    docker - image
    - added editline, albeit it's not used yet

---
+++ docker/share/images/editline/build-sources.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# extract sources and prepare for build
+tar -xf libedit-20170329-3.1.tar.gz
+cd libedit-20170329-3.1
+mkdir build && cd build
+
+PREFIX=/usr/local
+
+../configure \
+  --prefix=$PREFIX \
+  CFLAGS='-O3 -s' \
+  LDFLAGS='-Wl,-rpath,/usr/local/lib/,-rpath-link,/usr/local/lib/,-lncurses' \
+  CPPFLAGS='-I /usr/local/include/ncurses'
+
+# Calculates the optimal job count
+JOBS=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+make -j $JOBS && make install
+
+# make this image a package
+packageIncluding /usr/local/share/terminfo
