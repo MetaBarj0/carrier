f6576ed125f4463f19743112c4ffaaee02456619
MetaBarj0 <troctsch.cpp@gmail.com>
2018-02-09 13:49:15 +0100

    docker - image
    - created mysql_cli, the mysql client program with the bare minimum to
      run it. It was necessary for the update database features of
      trinitycore

---
+++ docker/share/images/mysql_cli/build-sources.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# not usual way to do things as I don't build anything from sources
+# Thus, I call some 'internal' functions not really intended to be used outside
+# the functions.sh script, but it does the job
+
+# manual inclusion of what I need, here, symlinks and libs
+echo '/usr/local/mysql/bin/mysql' >> /image.dist
+
+# follow up all symlinks if needed
+# resolve all dependencies of that symlink...
+collectSharedObjectDependencies
+
+include '/sur/share/terminfo'
+
+# finalizing the package
+finalizePackage
