d55d02fe735d4af05882c2760585c70161c3f44d
MetaBarj0 <troctsch.cpp@gmail.com>
2018-02-16 12:29:45 +0100

    docker - appliance - devenv_trinitycore
    - added clang-check and clang-format images
    - added aforementioned image as dependencies of the devenv_trinitycore
      shell service

---
+++ docker/share/images/clang-format/build-sources.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# 1- the repository name must exist in the environment
+if [ -z $REPOSITORY ]; then
+  echo 'Missing repository name...exiting...'
+  exit 1
+fi
+
+# not usual way to do things as I don't build anything from sources
+# Thus, I call some 'internal' functions not really intended to be used outside
+# the functions.sh script, but it does the job
+
+# manual inclusion of what I need
+include /usr/local/bin/clang-format
+
+# follow up all symlinks if needed
+# resolve all dependencies of that symlink...
+collectSharedObjectDependencies
+
+# finalizing the package
+finalizePackage
