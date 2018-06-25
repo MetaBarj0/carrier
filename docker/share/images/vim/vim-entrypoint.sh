99bb423fd1483460b49d40a61bbd3367767066dd
MetaBarj0 <troctsch.cpp@gmail.com>
2018-06-25 15:51:20 +0000

    image - vim
    - random fix, entrypoint is great now

--- docker/share/images/vim/vim-entrypoint.sh
+++ docker/share/images/vim/vim-entrypoint.sh
@@ -3,31 +3,7 @@
 # check arguments that are the command we want the container runs. if not set,
 # assumes that the user want a shell
 if [ -z "$1" ]; then
-  set -- sh;
-fi
-
-if [ -z "$VIM_LINUX_USER" ]; then
-  echo 'Hi there! I need a linux user id to execute my commands.'
-  echo 'Preferably, you should give me your current user id. Therefore, I can'
-  echo 'modify your files with the correct user, saving you a'
-  echo 'lot of pain in the a**.'
-  echo 'The next time you will run this container, please, provide a valid'
-  echo 'user id using the -e switch like :'
-  echo 'docker run --rm -it metabarj0/vim -e VIM_LINUX_USER=$(id -u)'
-  echo 'Bye!'
-  exit 1
-fi
-
-if [ -z "$VIM_LINUX_GROUP" ]; then
-  echo 'Hi there! I need a linux group id to execute my commands.'
-  echo 'Preferably, you should give me your current group id. Therefore, I can'
-  echo 'modify your files with the correct group, saving you a'
-  echo 'lot of pain in the a**.'
-  echo 'The next time you will run this container, please, provide a valid'
-  echo 'user id using the -e switch like :'
-  echo 'docker run --rm -it metabarj0/vim -e VIM_LINUX_GROUP=$(id -g)'
-  echo 'Bye!'
-  exit 1
+  set -- sh -l --;
 fi
 
 # Two phases execution :
@@ -39,19 +15,50 @@ fi
 # - second, as the vim user, execute this very same script and forward the
 #   provided command if any, otherwise, starts sh
 if [ $(whoami) != vim ]; then
+  if [ -z "$VIM_LINUX_USER" ]; then
+    echo 'Hi there! I need a linux user id to execute my commands.'
+    echo 'Preferably, you should give me your current user id. Therefore, I can'
+    echo 'modify your files with the correct user, saving you a'
+    echo 'lot of pain in the a**.'
+    echo 'The next time you will run this container, please, provide a valid'
+    echo 'user id using the -e switch like :'
+    echo 'docker run --rm -it -e VIM_LINUX_USER=$(id -u) metabarj0/vim'
+    echo 'Bye!'
+    exit 1
+  fi
+
+  if [ -z "$VIM_LINUX_GROUP" ]; then
+    echo 'Hi there! I need a linux group id to execute my commands.'
+    echo 'Preferably, you should give me your current group id. Therefore, I can'
+    echo 'modify your files with the correct group, saving you a'
+    echo 'lot of pain in the a**.'
+    echo 'The next time you will run this container, please, provide a valid'
+    echo 'user id using the -e switch like :'
+    echo 'docker run --rm -it -e VIM_LINUX_GROUP=$(id -g) metabarj0/vim'
+    echo 'Bye!'
+    exit 1
+  fi
+
   addgroup -g $VIM_LINUX_GROUP vim
   adduser -D -u $VIM_LINUX_USER -G vim vim
+
+  # security reasons, sensitive values
+  unset VIM_LINUX_USER
+  unset VIM_LINUX_GROUP
 
   # the subscript is only a call to this one but it expand here the value of
   # $@, avoiding to have to supply the 'su -c ...' command with arguments as
   # it does not work as intended
-  cat << EOI > vim-execute.sh
+  proxyScript='/home/vim/vim-execute.sh'
+  cat << EOI > "$proxyScript"
 #!/bin/sh
-exec entrypoint.sh "$@"
+rm -f $proxyScript
+exec vim-entrypoint.sh "$@"
 EOI
-  chmod +x vim-execute.sh
+  chmod +x "$proxyScript"
+  chown vim:vim "$proxyScript"
   
-  exec su vim -c ./vim-execute.sh
+  exec su vim -c "$proxyScript"
 # Here, i am the 'vim' user
 else
   exec $(echo "$@")
